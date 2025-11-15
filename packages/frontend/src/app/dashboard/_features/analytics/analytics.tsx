4c76abbb39d3ccfa6e95812a46ace07fe666df20
Ronjay Acorda <rgacorda.the2nd@gmail.com>
2025-11-15 11:38:20 +0800

    feat: moved insights to top

--- packages/frontend/src/app/dashboard/_features/analytics/analytics.tsx
+++ packages/frontend/src/app/dashboard/_features/analytics/analytics.tsx
@@ -2,23 +2,15 @@ import React from "react";
 import StatTable from "./components/statTable";
 import { ChartAreaInteractive } from "./components/chart-area";
 import MistakeAnalytics from "./components/mistakeAnalytics";
+import MistakeInsights from "./components/mistakeInsights";
 
 export default function AnalyticsDashboard() {
   return (
-    <div className="container mx-auto px-6">
-      <div className="mb-6">
-        <ChartAreaInteractive />
-      </div>
+    <div className="container mx-auto px-6 space-y-6">
+      <MistakeInsights />
+      <ChartAreaInteractive />
       <StatTable />
-      <div className="mt-8">
-        <MistakeAnalytics />
-      </div>
-      {/* <div className="grid grid-cols-2">
-        <StatCharts />
-        <StatCharts />
-        <StatCharts />
-        <StatCharts />
-    </div> */}
+      <MistakeAnalytics />
     </div>
   );
 }
